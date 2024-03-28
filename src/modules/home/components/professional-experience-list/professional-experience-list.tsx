4ccbfbfccafef5e99e5107b889cc5e8bec05056d
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2024-03-28 17:50:10 -0300

    feat: adds project carousel

--- src/modules/home/components/professional-experience-list/professional-experience-list.tsx
+++ src/modules/home/components/professional-experience-list/professional-experience-list.tsx
@@ -1,46 +1,30 @@
 import { BackgroundGradient } from "@shared/components/common";
+import { ProjectsCarousel, type ImageType } from "./projects-carousel";
 
-export function ProfessionalExperienceList() {
+type Props = {
+  projects: {
+    description: string;
+    images: ImageType[];
+  }[];
+};
+
+export function ProfessionalExperienceList({ projects }: Props) {
   return (
     <section className="flex flex-col gap-4 p-4">
-      <h2>Experiência Profissional</h2>
+      <h2 className="text-2xl font-black">Experiência Profissional</h2>
 
-      <div className="flex flex-col items-center justify-between gap-2 sm:flex-row">
-        <BackgroundGradient
-          className="h-[500px] rounded-xl bg-zinc-900 p-4"
-          containerClassName="p-2"
-        >
-          <div className="flex flex-col gap-4">
-            <h1 className="text-3xl font-bold">Serviços</h1>
-            <p className="text-xl font-semibold">
-              Desenvolvedor Fullstack, apaixonado por tecnologia e inovação.
-            </p>
-          </div>
-        </BackgroundGradient>
+      <div className="flex flex-col flex-wrap items-center justify-between gap-2 sm:flex-row sm:gap-6">
+        {projects.map((project, index) => (
+          <BackgroundGradient
+            key={index}
+            className="min-w-[250px] max-w-[350px] rounded-xl bg-zinc-900"
+            containerClassName="w-fit hover:-translate-y-0"
+          >
+            <ProjectsCarousel projects={project.images} />
 
-        <BackgroundGradient
-          className="h-[500px] rounded-xl bg-zinc-900 p-4"
-          containerClassName="p-2"
-        >
-          <div className="flex flex-col gap-4">
-            <h1 className="text-3xl font-bold">Serviços</h1>
-            <p className="text-xl font-semibold">
-              Desenvolvedor Fullstack, apaixonado por tecnologia e inovação.
-            </p>
-          </div>
-        </BackgroundGradient>
-
-        <BackgroundGradient
-          className="h-[500px] rounded-xl bg-zinc-900 p-4"
-          containerClassName="p-2"
-        >
-          <div className="flex flex-col gap-4">
-            <h1 className="text-3xl font-bold">Serviços</h1>
-            <p className="text-xl font-semibold">
-              Desenvolvedor Fullstack, apaixonado por tecnologia e inovação.
-            </p>
-          </div>
-        </BackgroundGradient>
+            <div className="p-4 text-justify">{project.description}</div>
+          </BackgroundGradient>
+        ))}
       </div>
     </section>
   );

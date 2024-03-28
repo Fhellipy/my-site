6b94b5ad15f8b942324e05a78cb0a47a223dbafd
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2024-03-28 17:50:16 -0300

    feat: adds project carousel

---
+++ src/modules/home/components/professional-experience-list/projects-carousel.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import Image from "next/image";
+import { useCallback } from "react";
+
+export type ImageType = {
+  id: number;
+  src: string;
+  alt: string;
+};
+
+type ProjectsCarouselProps = {
+  projects: ImageType[];
+};
+
+const OPTIONS = { loop: true };
+
+export function ProjectsCarousel({ projects }: ProjectsCarouselProps) {
+  const [emblaRef, emblaApi] = useEmblaCarousel(OPTIONS, [Autoplay()]);
+
+  const scrollPrev = useCallback(
+    () => emblaApi && emblaApi.scrollPrev(),
+    [emblaApi],
+  );
+
+  const scrollNext = useCallback(
+    () => emblaApi && emblaApi.scrollNext(),
+    [emblaApi],
+  );
+
+  return (
+    <div
+      className="relative w-full min-w-[250px] max-w-[85rem] overflow-hidden rounded-t-xl"
+      ref={emblaRef}
+    >
+      <div className="flex touch-pan-y">
+        {projects.map(project => (
+          <div
+            key={project.id}
+            className="relative min-w-0 flex-[100%] flex-shrink-0 flex-grow-0"
+          >
+            <Image
+              className="block max-h-96 min-h-[10rem] w-full object-cover"
+              src={project.src}
+              alt={project.alt}
+              width={300}
+              height={200}
+            />
+          </div>
+        ))}
+      </div>
+
+      <button
+        className="absolute bottom-2 right-14 inline-flex h-10 w-10 cursor-pointer touch-manipulation items-center justify-center rounded-full bg-muted text-muted-foreground shadow"
+        onClick={scrollPrev}
+      >
+        <ChevronLeftIcon
+          size={30}
+          className="flex-shrink-0 text-muted-foreground"
+        />
+      </button>
+
+      <button
+        className="absolute bottom-2 right-3 inline-flex h-10 w-10 cursor-pointer touch-manipulation items-center justify-center rounded-full bg-muted text-muted-foreground shadow"
+        onClick={scrollNext}
+      >
+        <ChevronRightIcon
+          size={30}
+          className="flex-shrink-0 text-muted-foreground"
+        />
+      </button>
+    </div>
+  );
+}

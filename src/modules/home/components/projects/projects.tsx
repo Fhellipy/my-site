38fdc4e50720c0ea1efad6d9bddfb39925ef89bc
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2024-03-28 17:51:13 -0300

    feat: adds project list

---
+++ src/modules/home/components/projects/projects.tsx
@@ -0,0 +1,47 @@
+import { BackgroundGradient } from "@shared/components/common";
+
+export function Projects() {
+  return (
+    <section className="flex flex-col gap-4 p-4">
+      <h2>Experiência Profissional</h2>
+
+      <div className="flex flex-col items-center justify-between gap-2 sm:flex-row">
+        <BackgroundGradient
+          className="h-[500px] rounded-xl bg-zinc-900 p-4"
+          containerClassName="p-2"
+        >
+          <div className="flex flex-col gap-4">
+            <h1 className="text-3xl font-bold">Serviços</h1>
+            <p className="text-xl font-semibold">
+              Desenvolvedor Fullstack, apaixonado por tecnologia e inovação.
+            </p>
+          </div>
+        </BackgroundGradient>
+
+        <BackgroundGradient
+          className="h-[500px] rounded-xl bg-zinc-900 p-4"
+          containerClassName="p-2"
+        >
+          <div className="flex flex-col gap-4">
+            <h1 className="text-3xl font-bold">Serviços</h1>
+            <p className="text-xl font-semibold">
+              Desenvolvedor Fullstack, apaixonado por tecnologia e inovação.
+            </p>
+          </div>
+        </BackgroundGradient>
+
+        <BackgroundGradient
+          className="h-[500px] rounded-xl bg-zinc-900 p-4"
+          containerClassName="p-2"
+        >
+          <div className="flex flex-col gap-4">
+            <h1 className="text-3xl font-bold">Serviços</h1>
+            <p className="text-xl font-semibold">
+              Desenvolvedor Fullstack, apaixonado por tecnologia e inovação.
+            </p>
+          </div>
+        </BackgroundGradient>
+      </div>
+    </section>
+  );
+}

72654e9c5ccd4e73cec8c6ad54f1684c95ad039d
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-04-06 23:03:31 -0300

    feat: adds data from the services and tools I use to develop

---
+++ src/modules/home/mock/services-&-tools-data.ts
@@ -0,0 +1,90 @@
+export const SERVICES_TOOLS_DATA = [
+  {
+    src: "/react.svg",
+    alt: "Logo do React",
+  },
+  {
+    src: "/typescript.svg",
+    alt: "Logo do TypeScript",
+  },
+  {
+    src: "/nextjs.svg",
+    alt: "Logo do Next.js",
+  },
+  {
+    src: "/node.svg",
+    alt: "Logo do Node.js",
+  },
+  {
+    src: "/javascript.svg",
+    alt: "Logo do JavaScript",
+  },
+  {
+    src: "/php.svg",
+    alt: "Logo do PHP",
+  },
+  {
+    src: "/golang.svg",
+    alt: "Logo do Golang",
+  },
+  {
+    src: "/nestjs.svg",
+    alt: "Logo do NestJS",
+  },
+  {
+    src: "/html5.svg",
+    alt: "Logo do HTML5",
+  },
+  {
+    src: "/css3.svg",
+    alt: "Logo do CSS3",
+  },
+  {
+    src: "/tailwindcss.svg",
+    alt: "Logo do Tailwind CSS",
+  },
+  {
+    src: "/firebird.svg",
+    alt: "Logo do Firebird",
+  },
+  {
+    src: "/postgresql.svg",
+    alt: "Logo do PostgreSQL",
+  },
+  {
+    src: "/mysql.svg",
+    alt: "Logo do MySQL",
+  },
+  {
+    src: "/mongodb.svg",
+    alt: "Logo do MongoDB",
+  },
+  {
+    src: "/docker.svg",
+    alt: "Logo do Docker",
+  },
+  {
+    src: "/storybook.svg",
+    alt: "Logo do Storybook",
+  },
+  {
+    src: "/json.svg",
+    alt: "Logo do JSON",
+  },
+  {
+    src: "/git.svg",
+    alt: "Logo do Git",
+  },
+  {
+    src: "/github.svg",
+    alt: "Logo do GitHub",
+  },
+  {
+    src: "/vscode.svg",
+    alt: "Logo do Visual Studio Code",
+  },
+  {
+    src: "/figma.svg",
+    alt: "Logo do Figma",
+  },
+];

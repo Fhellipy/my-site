d53fe52dbb994d572d6f2bc9dcb9b69496ef8da0
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-04-06 22:56:16 -0300

    fix: remove content that leaves the page under maintenance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,73 +7,8 @@ import {
   ProfessionalExperienceList,
   Tools,
 } from "@modules/home";
-import { TriangleAlertIcon } from "lucide-react";
-import Link from "next/link";
-import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
-
-const images = [
-  {
-    id: 1,
-    src: "https://via.placeholder.com/300x200",
-    alt: "Project 1",
-  },
-  {
-    id: 2,
-    src: "https://via.placeholder.com/300x200",
-    alt: "Project 2",
-  },
-];
 
 export default function Home() {
-  return (
-    <main className="flex h-svh flex-col items-center justify-center gap-6 bg-muted p-24 text-muted-foreground">
-      <div className="flex flex-col gap-2 rounded border px-6 py-4 text-center shadow">
-        <h1 className="text-2xl font-bold uppercase">Contato</h1>
-
-        <div className="flex flex-col gap-2 p-2">
-          <Link
-            target="_blank"
-            href="https://instagram.com/_fhellipy"
-            className="flex items-center gap-1 whitespace-nowrap hover:underline"
-          >
-            <BsInstagram size={22} /> @_fhellipy
-          </Link>
-
-          <Link
-            target="_blank"
-            href="https://www.linkedin.com/in/fhellipy-c-santana"
-            className="flex items-center gap-1 whitespace-nowrap hover:underline"
-          >
-            <BsLinkedin size={22} /> @fhellipy-c-santana
-          </Link>
-
-          <Link
-            target="_blank"
-            href="https://github.com/Fhellipy"
-            className="flex items-center gap-1 whitespace-nowrap hover:underline"
-          >
-            <BsGithub size={22} /> @Fhellipy
-          </Link>
-
-          <span className="flex items-center gap-1 whitespace-nowrap">
-            <p className="font-semibold">Email:</p>
-            <p>ffhellipyc.santana@gmail.com</p>
-          </span>
-        </div>
-      </div>
-
-      <div className="flex flex-col items-center text-center">
-        <TriangleAlertIcon className="h-24 w-24 text-yellow-500" />
-        <h1 className="text-3xl font-bold italic text-yellow-500">
-          Site em construção
-        </h1>
-      </div>
-
-      <p className="text-center text-xl font-semibold">
-        Tente novamente em breve!
-      </p>
-    </main>
-  );
   const items = [
     {
       id: "cards",
@@ -86,28 +21,7 @@ export default function Home() {
     },
     {
       id: "experiencia-profissional",
-      content: (
-        <ProfessionalExperienceList
-          projects={[
-            {
-              images,
-              description: "Descrição do projeto",
-            },
-            {
-              images,
-              description: "Descrição do projeto",
-            },
-            {
-              images,
-              description: "Descrição do projeto",
-            },
-            {
-              images,
-              description: "Descrição do projeto",
-            },
-          ]}
-        />
-      ),
+      content: <ProfessionalExperienceList />,
     },
     {
       id: "ferramentas",

c8e0b4c0aaf34a3c808924dc7eef243c7fc4f29d
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-04-06 23:00:24 -0300

    feat: adds dynamic data

--- src/modules/home/components/professional-experience-list/professional-experience-list.tsx
+++ src/modules/home/components/professional-experience-list/professional-experience-list.tsx
@@ -1,28 +1,37 @@
+import { PROFESSIONAL_EXPERIENCE_DATA } from "@modules/home";
 import { BackgroundGradient } from "@shared/components/common";
-import { ProjectsCarousel, type ImageType } from "./projects-carousel";
+import Link from "next/link";
+import { ProjectsCarousel } from "./projects-carousel";
 
-type Props = {
-  projects: {
-    description: string;
-    images: ImageType[];
-  }[];
-};
-
-export function ProfessionalExperienceList({ projects }: Props) {
+export function ProfessionalExperienceList() {
   return (
-    <section className="flex flex-col gap-4 p-4">
+    <section className="flex flex-col gap-4 px-6 py-8">
       <h2 className="text-2xl font-black">Experiência Profissional</h2>
 
       <div className="flex flex-col flex-wrap items-center justify-between gap-2 sm:flex-row sm:gap-6">
-        {projects.map((project, index) => (
+        {PROFESSIONAL_EXPERIENCE_DATA.map((project, index) => (
           <BackgroundGradient
             key={index}
-            className="min-w-[250px] max-w-[350px] rounded-xl bg-zinc-900"
-            containerClassName="w-fit hover:-translate-y-0"
+            className="min-w-[250px] max-w-[400px] rounded-xl bg-zinc-900"
+            containerClassName="w-fit"
           >
             <ProjectsCarousel projects={project.images} />
 
-            <div className="p-4 text-justify">{project.description}</div>
+            <div className="flex h-[400px] flex-col items-end gap-2 p-4">
+              <div className="w-full overflow-auto text-justify">
+                {project.description}
+              </div>
+
+              {project.link && (
+                <Link
+                  href={project.link}
+                  target="_blank"
+                  className="w-fit whitespace-nowrap rounded border-primary bg-primary px-4 py-2 font-bold hover:opacity-80"
+                >
+                  Saiba mais
+                </Link>
+              )}
+            </div>
           </BackgroundGradient>
         ))}
       </div>

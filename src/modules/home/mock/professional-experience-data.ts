cb776007260df64230b83000a843ab5ccd26974c
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-04-06 23:02:51 -0300

    feat: adds data from projects I have worked on professionally

---
+++ src/modules/home/mock/professional-experience-data.ts
@@ -0,0 +1,170 @@
+import BannerEcommerceDantasBikes from "@shared/assets/images/Banner-Ecommerce-Dantas-Bikes.png";
+import LoginEcommerceDantasBikes from "@shared/assets/images/Login-Ecommerce-Dantas-Bikes.png";
+import PaymentEcommerceDantasBikes from "@shared/assets/images/Payment-Ecommerce-Dantas-Bikes.png";
+import ProductEcommerceDantasBikes from "@shared/assets/images/Product-Ecommerce-Dantas-Bikes.png";
+import ProductListEcommerceDantasBikes from "@shared/assets/images/Product-list-Ecommerce-Dantas-Bikes.png";
+
+import Dashboard1DarkmodeElloSportsAdmin from "@shared/assets/images/Dashboard-1-darkmode-Ello-sports-admin.png";
+import Dashboard1LightmodeElloSportsAdmin from "@shared/assets/images/Dashboard-1-lightmode-Ello-sports-admin.png";
+import Dashboard2DarkmodeElloSportsAdmin from "@shared/assets/images/Dashboard-2-darkmode-Ello-sports-admin.png";
+import Dashboard2LightmodeElloSportsAdmin from "@shared/assets/images/Dashboard-2-lightmode-Ello-sports-admin.png";
+import Dashboard3DarkmodeElloSportsAdmin from "@shared/assets/images/Dashboard-3-darkmode-Ello-sports-admin.png";
+import LoginElloSportsAdmin from "@shared/assets/images/Login-Ello-sports-admin.png";
+
+import DashboardDarkmodeEcommerceAdmin from "@shared/assets/images/Dashboard-darkmode-Ecommerce-admin.png";
+import DashboardLightmodeEcommerceAdmin from "@shared/assets/images/Dashboard-lightmode-Ecommerce-admin.png";
+import LoginEcommerceAdmin from "@shared/assets/images/Login-Ecommerce-admin.png";
+import ProductsDarkmodeEcommerceAdmin from "@shared/assets/images/Products-darkmode-Ecommerce-admin.png";
+import ProductsLightmodeEcommerceAdmin from "@shared/assets/images/Products-lightmode-Ecommerce-admin.png";
+import StatisticsDarkmodeEcommerceAdmin from "@shared/assets/images/Statistics-darkmode-Ecommerce-admin.png";
+import StatisticsLightmodeEcommerceAdmin from "@shared/assets/images/Statistics-lightmode-Ecommerce-admin.png";
+
+import ChatSistemaDeChamadosInterno from "@shared/assets/images/Chat-sistema-de-chamados-interno.png";
+import DashboardSistemaDeChamadosInterno from "@shared/assets/images/Dashboard-sistema-de-chamados-interno.png";
+import DetailsSistemaDeChamadosInterno from "@shared/assets/images/Details-sistema-de-chamados-interno.png";
+import LoginSistemaDeChamadosInterno from "@shared/assets/images/Login-sistema-de-chamados-interno.png";
+import RegisterSistemaDeChamadosInterno from "@shared/assets/images/Register-sistema-de-chamados-interno.png";
+
+export const PROFESSIONAL_EXPERIENCE_DATA = [
+  {
+    images: [
+      {
+        id: 1,
+        src: LoginEcommerceDantasBikes.src,
+        alt: "Imagem do login do Dantas Bikes",
+      },
+      {
+        id: 2,
+        src: BannerEcommerceDantasBikes.src,
+        alt: "Imagem do banner de uma promoção do Dantas Bikes",
+      },
+      {
+        id: 3,
+        src: ProductListEcommerceDantasBikes.src,
+        alt: "Imagem da lista de produtos do Dantas Bikes",
+      },
+      {
+        id: 4,
+        src: ProductEcommerceDantasBikes.src,
+        alt: "Imagem do produto do Dantas Bikes",
+      },
+      {
+        id: 5,
+        src: PaymentEcommerceDantasBikes.src,
+        alt: "Imagem dos métodos de pagamentos do Dantas Bikes",
+      },
+    ],
+    description:
+      "Dantas Bikes é um site de e-commerce de uma loja de bicicletas e acessórios para bicicletas. Ele possui um layout que pode ser alterado através do seu painel administrativo. O site conta com um sistema de gerenciamento de produtos e pedidos, além de um sistema de pagamento integrado que aceita diversas formas de pagamento, incluindo dois cartões de crédito e PIX, e vários outros métodos de pagamento.",
+    link: "https://dantasbikes.com.br",
+  },
+
+  {
+    images: [
+      {
+        id: 1,
+        src: LoginEcommerceAdmin.src,
+        alt: "Imagem do login do Ecommerce admin",
+      },
+      {
+        id: 2,
+        src: DashboardDarkmodeEcommerceAdmin.src,
+        alt: "Imagem do dashboard do Ecommerce admin em darkmode",
+      },
+      {
+        id: 3,
+        src: DashboardLightmodeEcommerceAdmin.src,
+        alt: "Imagem do dashboard do Ecommerce admin em lightmode",
+      },
+      {
+        id: 4,
+        src: StatisticsDarkmodeEcommerceAdmin.src,
+        alt: "Imagem das estatísticas do Ecommerce admin em darkmode",
+      },
+      {
+        id: 5,
+        src: StatisticsLightmodeEcommerceAdmin.src,
+        alt: "Imagem das estatísticas do Ecommerce admin em lightmode",
+      },
+      {
+        id: 6,
+        src: ProductsDarkmodeEcommerceAdmin.src,
+        alt: "Imagem dos produtos do Ecommerce admin em darkmode",
+      },
+      {
+        id: 7,
+        src: ProductsLightmodeEcommerceAdmin.src,
+        alt: "Imagem dos produtos do Ecommerce admin em lightmode",
+      },
+    ],
+    description:
+      "Admin Dantas Bikes é um painel administrativo para gerenciar o site de e-commerce Dantas Bikes. Ele pode alterar o layout do site, gerenciar produtos e pedidos, criar promoções e cupons, e visualizar estatísticas de vendas. O painel possui um sistema de autenticação e autorização de usuários, e é responsivo, podendo ser acessado de qualquer dispositivo desde que tenha acesso à rede interna da empresa.",
+  },
+  {
+    images: [
+      {
+        id: 1,
+        src: LoginElloSportsAdmin.src,
+        alt: "Imagem do login do Ello sports admin",
+      },
+      {
+        id: 2,
+        src: Dashboard1DarkmodeElloSportsAdmin.src,
+        alt: "Imagem do dashboard 1 do Ello sports admin em darkmode",
+      },
+      {
+        id: 3,
+        src: Dashboard1LightmodeElloSportsAdmin.src,
+        alt: "Imagem do dashboard 1 do Ello sports admin em lightmode",
+      },
+      {
+        id: 4,
+        src: Dashboard2DarkmodeElloSportsAdmin.src,
+        alt: "Imagem do dashboard 2 do Ello sports admin em darkmode",
+      },
+      {
+        id: 5,
+        src: Dashboard2LightmodeElloSportsAdmin.src,
+        alt: "Imagem do dashboard 2 do Ello sports admin em lightmode",
+      },
+      {
+        id: 6,
+        src: Dashboard3DarkmodeElloSportsAdmin.src,
+        alt: "Imagem do dashboard 3 do Ello sports admin em darkmode",
+      },
+    ],
+    description:
+      "Ello sports Admin é um painel administrativo para gerenciar o site de venda de ingressos Ello sports desenvolvido para a empresa Dantas Eletro. Ele pode alterar o layout do site, gerenciar eventos e pedidos, e visualizar estatísticas de vendas. O painel possui um sistema de autenticação e autorização de usuários, e é responsivo, podendo ser acessado de qualquer dispositivo desde que tenha acesso à rede interna da empresa.",
+  },
+  {
+    images: [
+      {
+        id: 1,
+        src: LoginSistemaDeChamadosInterno.src,
+        alt: "Imagem do login do sistema de chamados interno",
+      },
+      {
+        id: 2,
+        src: DashboardSistemaDeChamadosInterno.src,
+        alt: "Imagem do dashboard do sistema de chamados interno",
+      },
+      {
+        id: 3,
+        src: DetailsSistemaDeChamadosInterno.src,
+        alt: "Imagem dos detalhes do sistema de chamados interno",
+      },
+      {
+        id: 4,
+        src: ChatSistemaDeChamadosInterno.src,
+        alt: "Imagem do chat do sistema de chamados interno",
+      },
+      {
+        id: 5,
+        src: RegisterSistemaDeChamadosInterno.src,
+        alt: "Imagem do registro do sistema de chamados interno",
+      },
+    ],
+    description:
+      "Sistema de Chamados Interno é um sistema de gerenciamento de chamados para empresa Dantas Eletro. Ele permite que os funcionários registrem chamados de suporte técnico, manutenção, e outros tipos de chamados. O sistema possui um chat interno para comunicação entre os funcionários e um painel administrativo para gerenciar os chamados. O sistema possui 3 sessões, sendo elas: a do administrador, a do técnico e a do usuário comum e cada sessão tem nível de acesso para as informações apresentadas. O sistema é responsivo, podendo ser acessado de qualquer dispositivo desde que tenha acesso à rede interna da empresa.",
+  },
+];

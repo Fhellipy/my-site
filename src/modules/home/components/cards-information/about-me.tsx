ae1dc0a4779eb45191d8e91f81d38c1253356a4e
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-04-06 22:58:35 -0300

    feat: adds description about me

--- src/modules/home/components/cards-information/about-me.tsx
+++ src/modules/home/components/cards-information/about-me.tsx
@@ -3,13 +3,39 @@ import { BackgroundGradient } from "@shared/components/common";
 export function AboutMe() {
   return (
     <BackgroundGradient
-      className="h-[450px] rounded-xl bg-zinc-900 p-4"
+      style={{
+        backgroundImage: "url(/wolf.jpeg)",
+        backgroundPosition: "bottom",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+      className="h-[450px] overflow-auto rounded-xl bg-zinc-900 p-4"
       containerClassName="p-2"
     >
-      <div id="sobre" className="flex flex-col gap-4">
-        <h1 className="text-center text-2xl font-bold uppercase">Sobre mim</h1>
-        <p className="text-center text-xl font-semibold">
-          Desenvolvedor Fullstack, apaixonado por tecnologia e inovação.
+      <div id="sobre" className="flex flex-col gap-2">
+        <h2 className="text-center text-2xl font-black uppercase">Sobre mim</h2>
+
+        <p className="text-center text-sm font-black text-secondary-foreground sm:text-justify sm:text-base">
+          Sou um desenvolvedor web FullStack apaixonado por tecnologia e
+          inovação. Minha formação como Técnico em Informática pelo IFBA e minha
+          atual graduação em Engenharia de Software pela Unicesumar me
+          proporcionaram uma base sólida para buscar constantemente
+          aperfeiçoamento em meus conhecimentos e habilidades técnicas.
+        </p>
+
+        <p className="text-center text-sm font-black text-secondary-foreground sm:text-justify sm:text-base">
+          Estou sempre em busca de novos desafios e oportunidades para aplicar
+          as melhores práticas e tecnologias no desenvolvimento de soluções web
+          excepcionais e dessa forma me tornar um desenvolvedor cada vez melhor.
+        </p>
+
+        <p className="text-center text-sm font-black text-secondary-foreground sm:text-justify sm:text-base">
+          Atualmente trabalho como desenvolvedor web usando ferramentas e
+          tecnologias como JavaScript, React.js, Next.js, Node.js, TypeScript,
+          PHP, Golang, TailwindCss, Stitches, Styled Components, Prisma,
+          PostgreSQL e Docker. Estou sempre disposto a aprender novas
+          tecnologias e ferramentas, e a compartilhar meus conhecimentos com a
+          comunidade.
         </p>
       </div>
     </BackgroundGradient>

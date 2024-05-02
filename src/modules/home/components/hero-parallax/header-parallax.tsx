4ee880e99d8a268736c586bd6ff7167b33a1b7a5
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-05-01 23:15:20 -0300

    feat: adds contacts information

--- src/modules/home/components/hero-parallax/header-parallax.tsx
+++ src/modules/home/components/hero-parallax/header-parallax.tsx
@@ -1,27 +1,64 @@
 import Image from "next/image";
+import Link from "next/link";
+import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
 
 export function HeaderParallax() {
   return (
-    <section className="relative left-0 top-0 mx-auto flex h-[--view-height] w-full items-center gap-4 bg-black p-6">
-      <div className="h-40 w-96 sm:h-96">
+    <section className="bg-custom-background relative left-0 top-0 mx-auto flex h-[48rem] w-full flex-col items-center justify-center gap-8 bg-cover bg-no-repeat object-cover p-6">
+      <span className="h-60 w-60 sm:h-80 sm:w-80">
         <Image
           src="/fhellipy.png"
-          width={300}
-          height={300}
+          width={500}
+          height={500}
           alt="Fhellipy Conceição"
-          className="h-full w-full rounded-full"
+          className="h-full w-full rounded-full object-cover"
         />
-      </div>
+      </span>
 
-      <div>
-        <h1 className="text-2xl font-black md:text-7xl">
-          Olá! Meu nome é Fhellipy Conceição
-        </h1>
-        <p className="mt-8 max-w-2xl text-base font-bold md:text-xl">
-          Sou desenvolvedor web Fullstack, apaixonado por tecnologia e inovação
-          que busca constantemente aperfeiçoamento em seus conhecimentos e
-          habilidades técnicas.
+      <div className="flex flex-col items-center justify-center gap-6">
+        <h1 className="text-3xl font-black md:text-5xl">Fhellipy Conceição</h1>
+        <p className="max-w-2xl border-b pb-4 text-xl font-bold md:text-2xl">
+          Desenvolvedor Web Fullstack
         </p>
+
+        <div id="contatos" className="mt-1 flex items-center gap-2 sm:gap-4">
+          <Link
+            target="_blank"
+            title="Instagram: @_fhellipy"
+            href="https://instagram.com/_fhellipy"
+            className="animate-bounce cursor-pointer rounded-full border p-4 hover:bg-muted"
+          >
+            <BsInstagram size={24} />
+          </Link>
+
+          <Link
+            target="_blank"
+            title="Linkedin: @fhellipy-c-santana"
+            href="https://www.linkedin.com/in/fhellipy-c-santana"
+            className="animate-bounce cursor-pointer rounded-full border p-4 hover:bg-muted"
+          >
+            <BsLinkedin size={24} />
+          </Link>
+
+          <Link
+            target="_blank"
+            title="Github: @fhellipy"
+            href="https://github.com/Fhellipy"
+            className="animate-bounce cursor-pointer rounded-full border p-4 hover:bg-muted"
+          >
+            <BsGithub size={24} />
+          </Link>
+
+          <Link
+            target="_blank"
+            title="Email: ffhellipyc.santana@gmail.com"
+            href="mailto:ffhellipyc.santana@gmail.com"
+            className="animate-bounce cursor-pointer rounded-full border p-4 hover:bg-muted"
+          >
+            <MdEmail size={24} />
+          </Link>
+        </div>
       </div>
     </section>
   );
